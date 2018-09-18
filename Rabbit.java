5dc98ef0a11a95be0a867099fd3f4a117f03622c
Wajiddat18c <43268743+Wajiddat18c@users.noreply.github.com>
2018-09-18 11:14:14 +0200

    Add files via upload

---
+++ Rabbit.java
@@ -0,0 +1,21 @@
+public class Rabbit{
+
+  //coordinates
+   private int x;
+   private int y;
+   
+   //konstruktør
+   public Rabbit(int x, int y){
+      this.x = x;
+      this.y = y;
+   }  
+   public void move(){
+      x++;
+      y++;   
+         System.out.println("Jeg er en Kanin, og står på feltet " + x + "," + y);
+}
+   public void help(){
+      System.out.println("NEEEJJ!!! Du må ikke spise mig Slange!");
+      }
+
+}
---
+++ Snake.java
@@ -0,0 +1,20 @@
+public class Snake{
+
+   private int x;
+   private int y;
+
+   public Snake(int x, int y){
+      this.x = x;
+      this.y = y;
+   }
+      public void move(){
+      x--;
+      y--;   
+         System.out.println("Jeg er en Slange, og står på feltet " + x + "," + y);
+}
+
+   public void eat(){
+      System.out.println("HAHA jeg spiser dig nu Kanin!");
+      System.out.println("HAPS!");
+}
+}
